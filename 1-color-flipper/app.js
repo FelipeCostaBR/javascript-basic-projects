a81744369ec9f05c25289a09a1dbf3a20c24e5d0
Felipe Costae <felipejsilvacosta@gmail.com>
2020-09-16 19:49:37 +1000

    basic js projects

---
+++ 1-color-flipper/app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "pink", "lavender", "BlueViolet", "gold"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    // get random number between 0-3 colors[0] 
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+ getRandomNumber = () =>  Math.floor(Math.random() * colors.length);
---
+++ 14-todo/app.js
@@ -0,0 +1,25 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.querySelector(".submit-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** EVENT LISTENERS **********
+form.addEventListener("submit", addItem);
+
+// ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+}
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
